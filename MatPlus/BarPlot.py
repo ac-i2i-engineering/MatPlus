4871092808abf3c64979edf0a8aa6367ad405bcb
BestLocation <115041500+BestLocation@users.noreply.github.com>
2025-02-09 14:52:23 -0500

    First two plot types

---
+++ MatPlus/BarPlot.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import plot;
+
+class BarPlot():
+
+    def __init__(self,x,y,lowerlimx = None,lowerlimy = None, upperlimx = None, upperlimy = None, wd = None, lw = None):
+        self.x = x
+        self.y = y
+        self.lowerlimx = lowerlimx
+        self.lowerlimy = lowerlimy
+        self.upperlimx = upperlimx
+        self.upperlimy = upperlimy
+        if self.lowerlimx == None:
+            self.lowerlimx = np.min(x) * 0.9
+        if self.lowerlimy == None:
+            self.lowerlimy = np.min(y) * 0.9
+        if self.upperlimx == None:
+            self.upperlimx = np.max(x) * 1.1
+        if self.upperlimy == None:
+            self.upperlimy = np.max(y) * 1.1
+        
+        self.width = wd
+        self.linewidth = lw
+
+        if self.linewidth == None:
+            self.linewidth = 1
+        if self.width == None:
+            self.width = 1
+    
+
+    def plot(self):
+        
+        plt.style.use('_mpl-gallery')
+        fig, ax = plt.subplots()
+        ax.bar(x, y, width=self.width, edgecolor="black", linewidth=1)
+        ax.set(xlim=(self.lowerlimx, self.upperlimx), xticks=np.arange(self.lowerlimx+1, self.upperlimx),
+            ylim=(self.lowerlimy, self.upperlimy), yticks=np.arange(self.lowerlimy+1, self.upperlimy))
+        plt.show()
+
+x = 0.5 + np.arange(8)
+y = [4.8, 5.5, 3.5, 4.6, 6.5, 6.6, 2.6, 3.0]
+barplot1 = BarPlot(x,y,None,None,None,None,1,1)
+barplot1.plot()
+
