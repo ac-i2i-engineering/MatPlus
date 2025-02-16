09a97fd0af4ccc29faa9b43dd1802afda714ced4
BestLocation <115041500+BestLocation@users.noreply.github.com>
2025-02-16 14:45:57 -0500

    Removed unnessary refrences

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,5 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from MatPlus.BarPlot import BarPlot
 from MatPlus.ScatterPlot import ScatterPlot
 
 __version__ = "0.0.1"
-__all__ = ['ScatterPlot', 'BarPlot']
+__all__ = ["ScatterPlot", "BarPlot"]
