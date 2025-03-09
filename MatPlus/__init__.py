11ca0c34c97aaca54f674de4f61f92c700a9c38a
liamjdavis <davisliam123@gmail.com>
2025-03-09 13:14:55 -0400

    Renamed pypi readme to be more specific and bumped version to 0.1.5

--- MatPlus/__init__.py
+++ MatPlus/__init__.py
@@ -4,5 +4,5 @@ from MatPlus.BoxPlot import BoxPlot
 from MatPlus.LinePlot import LinePlot
 from MatPlus.Histogram import Histogram
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = ["ScatterPlot", "BarPlot", "BoxPlot", "LinePlot", "Histogram"]
